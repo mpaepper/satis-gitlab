2fe7201aa50895fff09d63879fc319fe1e15816a
Marc Päpper <m.paepper@lemundo.de>
2018-10-29 18:16:49 +0000

    Added nightly sync

---
+++ nightlyFullSync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /var/www/satis.lemundo.de/web/satis-gitlab
+filename=.config.gitlab.api.key
+gitlab_api_key=$(head -n 1 $filename)
+
+bin/satis-gitlab gitlab-to-config \
+    --homepage http://satis.lemundo.de \
+    --output satis.json \
+    --template lemundo-template.json \
+    https://glab.lemundo.de $gitlab_api_key
+
+    #--gitlab-namespace 'Diverse - Gitolite' \ To filter for specific namespaces
+
+bin/satis-gitlab build satis.json web
